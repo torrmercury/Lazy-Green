b00ed5cefa71a087581494ff0f351ead71e50bf0
Johnny Mkitarian <firehyren@gmail.com>
2014-05-09 03:47:32 -0400

    GUI v2

--- Assets/Scripts/Gui_Menu.cs
+++ Assets/Scripts/Gui_Menu.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class Gui_Menu : MonoBehaviour {
 
 	public string text_here;
+	public string location_here;
+
+
 	// Use this for initialization
 	void Start () {
 	
@@ -16,26 +19,26 @@ public class Gui_Menu : MonoBehaviour {
 
 	void OnGUI() {
 		// Container Box
-		GUI.Box(new Rect(10,10,200,120), "Menu");
+		GUI.Box(new Rect(15,30,950,600), location_here);
 		
 		// Making buttons
-		if(GUI.Button(new Rect(20,40,80,20), "Button 1")) {
+		if(GUI.Button(new Rect(30,80,450,125), "Button 1")) {
 			Debug.Log ("button 1");
 		}
 		// Make the second button.
-		if(GUI.Button(new Rect(20,70,80,20), "Button 2")) {
+		if(GUI.Button(new Rect(30,215,450,125), "Button 2")) {
 			Debug.Log ("button 2");
 		}
 		// Repeat
-		if(GUI.Button(new Rect(120,40,80,20), "Button 3")) {
+		if(GUI.Button(new Rect(500,80,450,125), "Button 3")) {
 			Debug.Log ("button 3");
 		}
 		// Repeat
-		if(GUI.Button(new Rect(120,70,80,20), "Button 4")) {
+		if(GUI.Button(new Rect(500,215,450,125), "Button 4")) {
 			Debug.Log ("button 4");
 		}
 		//Text Area
-		GUI.Box(new Rect(15, 95, 190, 30), text_here);
+		GUI.Box(new Rect(25, 350, 925, 270), text_here);
 
 	}
 }
