7956339001147af725a69f8ea046b212d6e475f9
Johnny Mkitarian <firehyren@gmail.com>
2014-05-09 02:40:54 -0400

    bg_move script

---
+++ Assets/Scripts/bg_move.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bg_move : MonoBehaviour {
+
+	public int speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float translation = Input.GetAxis("Horizontal") * speed;
+		transform.Translate(translation, 0, 0);
+	}
+}
